453e5c4ef5787729e03567b9eb551a65e004f40f
Nathan Dunn <ndunn@me.com>
2020-12-05 07:29:32 -0800

    fixed a single test

--- grails-app/domain/org/xena/analysis/Gmt.groovy
+++ grails-app/domain/org/xena/analysis/Gmt.groovy
@@ -2,6 +2,15 @@ package org.xena.analysis
 
 class Gmt {
 
-    static constraints = {
-    }
+  static constraints = {
+  }
+
+  Long id
+  String name
+  String hash
+  String data
+
+  static mapping = {
+    data: 'text'
+  }
 }
--- src/test/groovy/org/xena/analysis/GmtControllerSpec.groovy
+++ src/test/groovy/org/xena/analysis/GmtControllerSpec.groovy
@@ -1,197 +1,196 @@
 package org.xena.analysis
 
-import spock.lang.*
-import static org.springframework.http.HttpStatus.OK
-import static org.springframework.http.HttpStatus.NOT_FOUND
-import static org.springframework.http.HttpStatus.NO_CONTENT
-import static org.springframework.http.HttpStatus.CREATED
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
-import grails.validation.ValidationException
-import grails.testing.web.controllers.ControllerUnitTest
-import grails.testing.gorm.DomainUnitTest
 import grails.plugin.json.view.JsonViewGrailsPlugin
+import grails.testing.gorm.DomainUnitTest
+import grails.testing.web.controllers.ControllerUnitTest
+import grails.validation.ValidationException
+import spock.lang.Specification
+
+import static org.springframework.http.HttpStatus.*
 
 class GmtControllerSpec extends Specification implements ControllerUnitTest<GmtController>, DomainUnitTest<Gmt> {
 
-    void setupSpec() {
-        defineBeans(new JsonViewGrailsPlugin(applicationContext: applicationContext))
+  void setupSpec() {
+    defineBeans(new JsonViewGrailsPlugin(applicationContext: applicationContext))
+  }
+
+  def populateValidParams(params) {
+    assert params != null
+
+    // TODO: Populate valid properties like...
+    params["name"] = 'someValidName'
+    params["data"] = 'data'
+    params["hash"] = 'hash'
+    params["id"] = 7
+  }
+
+  void "Test the index action returns the correct response"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * list(_) >> []
+      1 * count() >> 0
     }
 
-    def populateValidParams(params) {
-        assert params != null
+    when: "The index action is executed"
+    controller.index()
 
-        // TODO: Populate valid properties like...
-        //params["name"] = 'someValidName'
-        assert false, "TODO: Provide a populateValidParams() implementation for this generated test suite"
+    then: "The response is correct"
+    response.text == '[]'
+  }
+
+
+  void "Test the save action with a null instance"() {
+    when:
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'POST'
+    controller.save()
+
+    then:
+    response.status == UNPROCESSABLE_ENTITY.value()
+  }
+
+  void "Test the save action correctly persists"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * save(_ as Gmt)
     }
 
-    void "Test the index action returns the correct response"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * list(_) >> []
-            1 * count() >> 0
-        }
+    when:
+    response.reset()
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'POST'
+    populateValidParams(params)
+    request.json = new Gmt(params)
+    controller.save()
 
-        when:"The index action is executed"
-            controller.index()
+    then:
+    response.status == CREATED.value()
+    response.json
+  }
 
-        then:"The response is correct"
-            response.text == '[]'
+  void "Test the save action with an invalid instance"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * save(_ as Gmt) >> { Gmt gmt ->
+        throw new ValidationException("Invalid instance", gmt.errors)
+      }
     }
 
+    when:
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'POST'
+    populateValidParams(params)
+    request.json = new Gmt(params)
+    controller.save()
 
-    void "Test the save action with a null instance"() {
-        when:
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'POST'
-        controller.save()
+    then:
+    response.status == UNPROCESSABLE_ENTITY.value()
+    response.json
+  }
 
-        then:
-        response.status == UNPROCESSABLE_ENTITY.value()
+  void "Test the show action with a null id"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * get(null) >> null
     }
 
-    void "Test the save action correctly persists"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * save(_ as Gmt)
-        }
+    when: "The show action is executed with a null domain"
+    controller.show()
 
-        when:
-        response.reset()
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'POST'
-        populateValidParams(params)
-        request.json = new Gmt(params)
-        controller.save()
+    then: "A 404 error is returned"
+    response.status == NOT_FOUND.value()
+  }
 
-        then:
-        response.status == CREATED.value()
-        response.json
+  void "Test the show action with a valid id"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * get(2) >> new Gmt()
     }
 
-    void "Test the save action with an invalid instance"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * save(_ as Gmt) >> { Gmt gmt ->
-                throw new ValidationException("Invalid instance", gmt.errors)
-            }
-        }
+    when: "A domain instance is passed to the show action"
+    params.id = 2
+    controller.show()
 
-        when:
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'POST'
-        populateValidParams(params)
-        request.json = new Gmt(params)
-        controller.save()
+    then: "A model is populated containing the domain instance"
+    response.status == OK.value()
+    response.json == [:]
+  }
 
-        then:
-        response.status == UNPROCESSABLE_ENTITY.value()
-        response.json
+  void "Test the update action with a null instance"() {
+    when:
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'PUT'
+    controller.update()
+
+    then:
+    response.status == UNPROCESSABLE_ENTITY.value()
+  }
+
+  void "Test the update action correctly persists"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * save(_ as Gmt)
     }
 
-    void "Test the show action with a null id"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * get(null) >> null
-        }
+    when:
+    response.reset()
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'PUT'
+    populateValidParams(params)
+    def instance = new Gmt(params)
+    instance.id = 1
+    instance.version = 0
+    controller.update(instance)
 
-        when:"The show action is executed with a null domain"
-        controller.show()
+    then:
+    response.status == OK.value()
+    response.json
+  }
 
-        then:"A 404 error is returned"
-        response.status == NOT_FOUND.value()
+  void "Test the update action with an invalid instance"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * save(_ as Gmt) >> { Gmt gmt ->
+        throw new ValidationException("Invalid instance", gmt.errors)
+      }
     }
 
-    void "Test the show action with a valid id"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * get(2) >> new Gmt()
-        }
+    when:
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'PUT'
+    def instance = new Gmt(params)
+    instance.id = 1
+    instance.version = 0
+    controller.update(instance)
 
-        when:"A domain instance is passed to the show action"
-        params.id = 2
-        controller.show()
+    then:
+    response.status == UNPROCESSABLE_ENTITY.value()
+    response.json
+  }
 
-        then:"A model is populated containing the domain instance"
-        response.status == OK.value()
-        response.json == [:]
+  void "Test the delete action with a null instance"() {
+    when:
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'DELETE'
+    controller.delete()
+
+    then:
+    response.status == NOT_FOUND.value()
+  }
+
+  void "Test the delete action with an instance"() {
+    given:
+    controller.gmtService = Mock(GmtService) {
+      1 * delete(2) >> new Gmt(id: 2)
     }
 
-    void "Test the update action with a null instance"() {
-        when:
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'PUT'
-        controller.update()
+    when:
+    request.contentType = JSON_CONTENT_TYPE
+    request.method = 'DELETE'
+    params.id = 2
+    controller.delete()
 
-        then:
-        response.status == UNPROCESSABLE_ENTITY.value()
-    }
-
-    void "Test the update action correctly persists"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * save(_ as Gmt)
-        }
-
-        when:
-        response.reset()
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'PUT'
-        populateValidParams(params)
-        def instance = new Gmt(params)
-        instance.id = 1
-        instance.version = 0
-        controller.update(instance)
-
-        then:
-        response.status == OK.value()
-        response.json
-    }
-
-    void "Test the update action with an invalid instance"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * save(_ as Gmt) >> { Gmt gmt ->
-                throw new ValidationException("Invalid instance", gmt.errors)
-            }
-        }
-
-        when:
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'PUT'
-        def instance = new Gmt(params)
-        instance.id = 1
-        instance.version = 0
-        controller.update(instance)
-
-        then:
-        response.status == UNPROCESSABLE_ENTITY.value()
-        response.json
-    }
-
-    void "Test the delete action with a null instance"() {
-        when:
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'DELETE'
-        controller.delete()
-
-        then:
-        response.status == NOT_FOUND.value()
-    }
-
-    void "Test the delete action with an instance"() {
-        given:
-        controller.gmtService = Mock(GmtService) {
-            1 * delete(2) >> new Gmt(id: 2)
-        }
-
-        when:
-        request.contentType = JSON_CONTENT_TYPE
-        request.method = 'DELETE'
-        params.id = 2
-        controller.delete()
-
-        then:
-        response.status == NO_CONTENT.value()
-    }
+    then:
+    response.status == NO_CONTENT.value()
+  }
 }
