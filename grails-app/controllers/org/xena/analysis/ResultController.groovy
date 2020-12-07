d46aaf89ff806c03297c063396ea15f45bd19a43
Nathan Dunn <ndunn@me.com>
2020-12-07 09:32:50 -0800

    started added the proper methods

--- grails-app/controllers/org/xena/analysis/ResultController.groovy
+++ grails-app/controllers/org/xena/analysis/ResultController.groovy
@@ -1,5 +1,6 @@
 package org.xena.analysis
 
+import grails.converters.JSON
 import grails.validation.ValidationException
 import static org.springframework.http.HttpStatus.CREATED
 import static org.springframework.http.HttpStatus.NOT_FOUND
@@ -27,10 +28,88 @@ class ResultController {
         respond resultService.get(id)
     }
 
-    @Transactional
-    def save(Result result) {
-        if (result == null) {
-            render status: NOT_FOUND
+
+  Tpm generateTpmFromCohort(Object o) {
+    null
+  }
+
+  Gmt generateGmtFile(Object o1, Object o2) {
+    null
+  }
+
+  File generateEmptyAnalysisFile(Gmt gmt, Object o) {
+    null
+  }
+
+  void checkAnalysisEnvironment() {
+
+  }
+
+  @Transactional
+  def analyze() {
+    def json = request.JSON
+    prinltn "input json ${json as JSON}"
+    def cohort = json.cohort
+    def genesetName = json.genesetName
+    def gmtData = json.gmtData
+
+    Tpm tpmFile = generateTpmFromCohort(cohort)
+    Gmt gmtPath = generateGmtFile(genesetName,gmtData) // TODO: write to file
+    def outputFile = this.generateEmptyAnalysisFile(gmtPath,cohort) // TODO: write an output file based on hash of geneset and cohort
+
+    this.checkAnalysisEnvironment()
+//    console.log(`analysis environmeent fine "${method}"`)
+//    if(method==='BPA'){
+//      if(fs.existsSync(outputFile) && fs.statSync(outputFile).size == 0 ){
+//        console.log(`exists and is blank`)
+//        fs.unlinkSync(outputFile)
+//      }
+//      if(!fs.existsSync(outputFile)){
+//        console.log('running BPA')
+//        this.runBpaAnalysis(gmtPath,tpmFile,outputFile)
+//        console.log('RAN BPA')
+//      }
+//    }
+//    else{
+//      console.log('methid is not BPA ? ',method)
+//    }
+//    console.log('reading file')
+//    const result = await fs.readFileSync(outputFile,"utf8")
+//    console.log('read file')
+
+//    const convertedResult = this.convertTsv(result)
+//    console.log('adding gene sets to results')
+//    console.log('result',result)
+//    this.addGeneSetResult(method,genesetName,convertedResult)
+//    console.log('added result')
+//    this.saveGeneSetState(DEFAULT_PATH)
+//    console.log('saved gene state')
+//    if (result == null) {
+//      render status: NOT_FOUND
+//      return
+//    }
+//    if (result.hasErrors()) {
+//      transactionStatus.setRollbackOnly()
+//      respond result.errors
+//      return
+//    }
+//
+//    try {
+//      resultService.save(result)
+//    } catch (ValidationException e) {
+//      respond result.errors
+//      return
+//    }
+//
+//    respond result, [status: CREATED, view:"show"]
+    Result result = new Result()
+    save(result)
+  }
+
+  @Transactional
+  def save(Result result) {
+    if (result == null) {
+      render status: NOT_FOUND
             return
         }
         if (result.hasErrors()) {
