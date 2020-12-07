d5026aacc2071bab2cebf8de8c935a58cbf77653
Nathan Dunn <ndunn@me.com>
2020-12-07 14:11:56 -0800

    added an initial save

--- grails-app/controllers/org/xena/analysis/ResultController.groovy
+++ grails-app/controllers/org/xena/analysis/ResultController.groovy
@@ -2,6 +2,11 @@ package org.xena.analysis
 
 import grails.converters.JSON
 import grails.validation.ValidationException
+import org.grails.web.json.JSONArray
+import org.grails.web.json.JSONObject
+
+import java.security.MessageDigest
+
 import static org.springframework.http.HttpStatus.CREATED
 import static org.springframework.http.HttpStatus.NOT_FOUND
 import static org.springframework.http.HttpStatus.NO_CONTENT
@@ -17,9 +22,10 @@ class ResultController {
     ResultService resultService
 
     static responseFormats = ['json', 'xml']
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+//    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE",analyze: "POST", checkAnalysisEnvironment: "GET"]
 
     def index(Integer max) {
+      println "number is ${max}"
         params.max = Math.min(max ?: 10, 100)
         respond resultService.list(params), model:[resultCount: resultService.count()]
     }
@@ -28,34 +34,68 @@ class ResultController {
         respond resultService.get(id)
     }
 
+  def test(){
+    println "just testing"
+    render new JSONArray() as JSON
+  }
 
   Tpm generateTpmFromCohort(Object o) {
     null
   }
 
   Gmt generateGmtFile(Object o1, Object o2) {
-    null
   }
 
   File generateEmptyAnalysisFile(Gmt gmt, Object o) {
     null
   }
 
-  void checkAnalysisEnvironment() {
+  def checkAnalysisEnvironment() {
+
+    render new JSONObject() as JSON
 
   }
 
+
   @Transactional
   def analyze() {
+    println "doing analyze"
     def json = request.JSON
-    prinltn "input json ${json as JSON}"
-    def cohort = json.cohort
-    def genesetName = json.genesetName
-    def gmtData = json.gmtData
+    println "input json ${json as JSON}"
+    String cohort = json.cohort
+    String gmtname = json.gmtname
+    String gmtdata = json.gmtdata
+    String tpmUrl = json.tpmurl
 
-    Tpm tpmFile = generateTpmFromCohort(cohort)
-    Gmt gmtPath = generateGmtFile(genesetName,gmtData) // TODO: write to file
-    def outputFile = this.generateEmptyAnalysisFile(gmtPath,cohort) // TODO: write an output file based on hash of geneset and cohort
+
+    // handle and write gmt file
+    String gmtDataHash = gmtdata.md5()
+    Gmt gmt = Gmt.findByName(gmtname)
+    if(gmt==null){
+      gmt = new Gmt(name:gmtname,hash:gmtDataHash,data: gmtdata)
+      gmt.save(failOnError: true, flush: true)
+    }
+    File gmtFile = File.createTempFile(gmtname,"gmt")
+    gmtFile.write(gmtdata)
+    gmtFile.deleteOnExit()
+
+
+    // handl and write tpm file
+    Tpm tpm = Tpm.findByCohort(cohort)
+    if(tpm == null){
+      String tpmData = new URL(tpmUrl).text
+      tpm = new Tpm(
+        cohort: cohort,
+        url: tpmUrl,
+        data: tpmData
+      ).save(failOnError: true, flush: true)
+    }
+
+
+    // create output file
+    File outputFile = File.createTempFile("output-${cohort.replaceAll("[ |\\(|\\)]",'_')}${hash}","tsv")
+    outputFile.write("")
+//    def outputFile = this.generateEmptyAnalysisFile(gmtFile,cohort) // TODO: write an output file based on hash of geneset and cohort
 
     this.checkAnalysisEnvironment()
 //    console.log(`analysis environmeent fine "${method}"`)
@@ -104,6 +144,8 @@ class ResultController {
 //    respond result, [status: CREATED, view:"show"]
     Result result = new Result()
     save(result)
+
+    render new JSONObject() as JSON
   }
 
   @Transactional
--- grails-app/controllers/xena/analysis/grails/UrlMappings.groovy
+++ grails-app/controllers/xena/analysis/grails/UrlMappings.groovy
@@ -2,16 +2,26 @@ package xena.analysis.grails
 
 class UrlMappings {
 
-    static mappings = {
-        delete "/$controller/$id(.$format)?"(action:"delete")
-        get "/$controller(.$format)?"(action:"index")
-        get "/$controller/$id(.$format)?"(action:"show")
-        post "/$controller(.$format)?"(action:"save")
-        put "/$controller/$id(.$format)?"(action:"update")
-        patch "/$controller/$id(.$format)?"(action:"patch")
+  static mappings = {
 
-        "/"(controller: 'application', action:'index')
-        "500"(view: '/error')
-        "404"(view: '/notFound')
+
+    delete "/$controller/$id(.$format)?"(action: "delete")
+    get "/$controller(.$format)?"(action: "index")
+    get "/$controller/$id(.$format)?"(action: "show")
+    post "/$controller(.$format)?"(action: "save")
+    put "/$controller/$id(.$format)?"(action: "update")
+    patch "/$controller/$id(.$format)?"(action: "patch")
+
+    "/$controller/$action?/$id?"{
+      constraints {
+        // apply constraints here
+      }
     }
+
+//    "/result/test"(controller: 'result', action: 'test')
+//    "/result/analyze"(controller: 'result', action: 'analyze')
+    "/"(controller: 'application', action: 'index')
+    "500"(view: '/error')
+    "404"(view: '/notFound')
+  }
 }
