58a924766866dfccbbacb0fdd1b01fc96609f0c1
Nathan Dunn <ndunn@me.com>
2020-12-28 18:42:36 -0800

    updated test

--- src/test/groovy/org/xena/analysis/AnalysisServiceSpec.groovy
+++ src/test/groovy/org/xena/analysis/AnalysisServiceSpec.groovy
@@ -145,9 +145,7 @@ class AnalysisServiceSpec extends Specification implements ServiceUnitTest<Analy
     expect:
     def gmtData = new File("src/test/data/gmtData.gmt").text
     def meanMap = new JSONObject(new File("src/test/data/meanMap.json").text) as Map
-    println "0"
     JSONArray outputArray = AnalysisService.generateResult(gmtData,meanMap)
-    println "1"
     assert outputArray.length()==9
 
 
