82c4325ce8b16e8368af9859cc928e44c85f20c7
Nathan Dunn <ndunn@me.com>
2021-01-28 09:18:52 -0800

    cleaned up method a bit

--- grails-app/domain/org/xena/analysis/Gmt.groovy
+++ grails-app/domain/org/xena/analysis/Gmt.groovy
@@ -9,27 +9,12 @@ class Gmt {
   int geneSetCount
   int availableTpmCount // count from defaultGeneSet on initial load
 
-//  Double mean  // deprecated
-//  Double variance // deprecated
   // use gene names as key
   String stats // { 'ABC':{mean: 0.11212, std: 0.272 },  'DEF': { mean:0.17, std:0.3 } }
 
-  Boolean ready(){
-    return availableTpmCount == getLoadedResultCount() && stats != null
-  }
-
-  int getLoadedResultCount(){
-//    return results!=null ? results.size() : 0
-    return 33
-  }
-
   static constraints = {
     name blank: false, unique: true
-//    variance nullable: true
-//    mean nullable: true
     stats nullable: true
-//    hash blank: false, unique: true
-//    data blank: false, unique: true
   }
 
   static mapping = {
@@ -38,7 +23,6 @@ class Gmt {
   }
 
   static hasMany = [
-//    results: Result,
     results: TpmGmtResult,
   ]
 }
