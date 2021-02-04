05f9df2874969176d9d46c6c299fd9b02d8e22a2
Nathan Dunn <ndunn@me.com>
2021-02-04 11:15:12 -0800

    converted default json to gmt

--- src/test/groovy/org/xena/analysis/GmtSpec.groovy
+++ src/test/groovy/org/xena/analysis/GmtSpec.groovy
@@ -1,6 +1,9 @@
 package org.xena.analysis
 
+import grails.converters.JSON
 import grails.testing.gorm.DomainUnitTest
+import org.grails.web.json.JSONArray
+import org.grails.web.json.JSONObject
 import spock.lang.Specification
 
 class GmtSpec extends Specification implements DomainUnitTest<Gmt> {
@@ -11,8 +14,37 @@ class GmtSpec extends Specification implements DomainUnitTest<Gmt> {
     def cleanup() {
     }
 
-    void "test something"() {
-//        expect:"fix me"
-//            true == false
+    void "convert default BPA"() {
+      given:
+      File inputFile = new File("src/test/data/defaultBpaGeneSet.json")
+      File outputFile = new File("src/test/data/defaultBpaGeneSet.gmt")
+      outputFile.write("")
+      JSONArray geneSetArray = JSON.parse(inputFile.text)
+      println "# of gene sets ${geneSetArray.size()}"
+
+      when:
+      int count = 0
+      for(def obj in geneSetArray){
+//        println "processing ${obj.golabel}"
+//        println "genes is ${obj.gene.size()}"
+        List<String> lineList = new ArrayList<>()
+        lineList.add(obj.golabel)
+        lineList.add(obj.goid ?: "")
+        for(def gene in obj.gene){
+          lineList.add(gene)
+        }
+        String outputString = lineList.join("\t")+"\n"
+//        outputFile.write(lineList.join("\t"))
+//        outputFile.write("\n")
+        outputFile.append(outputString)
+        ++count
+      }
+      println "last gene set written: $count"
+
+
+      then:
+      assert count==geneSetArray.size()
+//      assert outputFile.text.readLines().size()==geneSetArray.size()
+
     }
 }
