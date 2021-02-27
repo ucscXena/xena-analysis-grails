f3ebcef47b82631786a3f0c16e8ad68c5818cca5
Nathan Dunn <nathandunn@lbl.gov>
2021-02-26 20:09:41 -0800

    fixes #47

--- grails-app/services/org/xena/analysis/UserService.groovy
+++ grails-app/services/org/xena/analysis/UserService.groovy
@@ -1,18 +1,5 @@
 package org.xena.analysis
 
-//import com.nimbusds.jose.JWSSigner
-//import com.nimbusds.jose.JWSVerifier
-//import com.nimbusds.jose.crypto.RSASSASigner
-//import com.nimbusds.jose.crypto.RSASSAVerifier
-//import com.nimbusds.jose.jwk.RSAKey
-//import com.nimbusds.jose.jwk.gen.RSAKeyGenerator
-//import com.nimbusds.jwt.JWT
-//import com.nimbusds.jwt.JWTParser
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
-import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.nimbusds.jwt.SignedJWT
 import grails.gorm.transactions.Transactional
 import org.grails.web.json.JSONObject
@@ -36,29 +23,7 @@ class UserService {
     AuthenticatedUser getUserFromRequest(HttpServletRequest httpServletRequest) {
 
         String authHeader = httpServletRequest.getHeader('Authorization')
-        println "authHeader"
-        println authHeader
         String jwtString = authHeader.split("jwt=")[1]
-        String accessToken = httpServletRequest.getHeader('GoogleAccessToken')
-        println "accessToken"
-        println accessToken
-
-//        // TODO:
-//        // from https://connect2id.com/products/nimbus-jose-jwt/examples/jwt-with-rsa-signature
-//        //
-//        JWT jwt = JWTParser.parse(jwtString)
-//        println "is signed? ${jwt instanceof SignedJWT}"
-//        RSAKey rsaJWK = new RSAKeyGenerator(2048)
-//                .keyID(System.getenv("GOOGLE_ID"))
-//                .generate();
-//        RSAKey rsaPublicJWK = rsaJWK.toPublicJWK();
-//// Create RSA-signer with the private key
-//        JWSSigner signer = new RSASSASigner(rsaJWK);
-//        JWSVerifier verifier = new RSASSAVerifier(rsaPublicJWK);
-//        Boolean validated  = signedJWT.verify(verifier)
-//        assertTrue(signedJWT.verify(verifier));
-//        println "is valid token: ${validated}"
-
         SignedJWT signedJWT = SignedJWT.parse(jwtString);
         def claimObject = new JSONObject(signedJWT.getJWTClaimsSet().toJSONObject())
         Boolean isEmailVerified = claimObject.getBoolean("email_verified")
@@ -66,15 +31,17 @@ class UserService {
             throw new RuntimeException("Not validated")
         }
 
-        // TODO: do stuff to extract the token and clean stuff up
-//        def tokens = jwtString.split("\\.")
-//        println tokens.length
-//        def headers =  new String(java.util.Base64.decoder.decode(tokens[1]))
-//        println headers
-//        def jsonObject = new JSONObject(headers)
+        def validationText = new URL("https://oauth2.googleapis.com/tokeninfo?id_token=${jwtString}").text
+        def validationObject = new JSONObject(validationText)
+        if(!validationObject.email_verified || validationObject.email != claimObject["email"] ){
+            throw new RuntimeException("Invalid signatuture for key: "+validationText)
+        }
+        else{
+            println "valid signature"
+        }
 
-
-        // TODO: need to get the access  token to work here
+//        // TODO: need to get the access  token to work here
+//        https://developers.google.com/identity/sign-in/web/backend-auth
 //        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
 //        // Specify the CLIENT_ID of the app that accesses the backend:
 //                .setAudience(Collections.singletonList(CLIENT_ID))
